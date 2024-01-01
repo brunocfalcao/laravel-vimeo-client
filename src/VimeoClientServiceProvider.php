96bfd26ffaec2a2fbd8180ef34ef73a767eb2bab
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-01-01 12:43:16 +0100

    Initial commit

---
+++ src/VimeoClientServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\VimeoClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class VimeoClientServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
